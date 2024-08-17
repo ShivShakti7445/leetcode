6c27de6f06a8324f61485996ac3fe4469abc852f
Shiv Shakti <141945568+ShivShakti7445@users.noreply.github.com>
2024-08-18 04:35:42 +0530

    Time: 0 ms (100%), Space: 41.8 MB (25.43%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.java
@@ -18,30 +18,25 @@ class Solution {
         return constructBST(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE, new int[]{0});
     }
     
-    private TreeNode constructBST(int[] preorder, int min, int max, int[] index) {
-        // Base case: if index is out of bounds, return null
-        if (index[0] == preorder.length) {
-            return null;
-        }
-        
-        int val = preorder[index[0]];
-        
-        // If current value does not fit the range [min, max], return null
-        if (val < min || val > max) {
-            return null;
-        }
-        
-        // Increment index for the next recursion
-        index[0]++;
-        
-        // Create the root node with the current value
-        TreeNode root = new TreeNode(val);
-        
-        // Recursively construct the left and right subtrees
-        root.left = constructBST(preorder, min, val, index);   // Left subtree must have values less than val
-        root.right = constructBST(preorder, val, max, index);  // Right subtree must have values greater than val
-        
-        return root;
+   private TreeNode constructBST(int[] preorder,int min,int max,int[]index){
+    if(index[0]== preorder.length){
+        return null;
     }
+    
+    
+    int val=preorder[index[0]];
+
+    if(val<min||val>max){
+        return null;
+    }
+
+    index[0]++;
+
+    TreeNode root=new TreeNode(val);
+    root.left=constructBST(preorder, min,val,index);
+    root.right=constructBST(preorder,val,max,index);
+
+    return root;
+   }
     
 }
