a96d3e67a893ff920bc933777913ee03eca6ddd2
Shiv Shakti <141945568+ShivShakti7445@users.noreply.github.com>
2024-10-04 23:17:57 +0530

    Time: 8 ms (35.17%), Space: 45.2 MB (52.87%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -1,25 +1,56 @@
+// class Solution {
+//     public int minFallingPathSum(int[][] matrix) {
+//          int n=matrix.length;
+//          int dp[][]=new int[n][n];
+//          for(int j=0;j<n;j++){
+//             dp[n-1][j]=matrix[n-1][j];
+//          }
+//          for(int i=n-2;i>=0;i--){
+//             int dyl=Integer.MAX_VALUE;
+//             int dyr=Integer.MAX_VALUE;
+//             for(int j=n-1;j>=0;j--){
+//                  int d=matrix[i][j]+dp[i+1][j];
+//                  if(j>0)dyl=matrix[i][j]+dp[i+1][j-1];
+//                  if(j<n-1)dyr=matrix[i][j]+dp[i+1][j+1];
+
+//                 dp[i][j]=Math.min(d,Math.min(dyl,dyr));
+//             }
+//          } 
+//          int min=Integer.MAX_VALUE;
+//          for(int j=0;j<n;j++){
+//             min=Math.min(min,dp[0][j]);
+//          }
+//         return min;
+//     }
+// }
+
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-         int n=matrix.length;
-         int dp[][]=new int[n][n];
-         for(int j=0;j<n;j++){
-            dp[n-1][j]=matrix[n-1][j];
-         }
-         for(int i=n-2;i>=0;i--){
-            int dyl=Integer.MAX_VALUE;
-            int dyr=Integer.MAX_VALUE;
-            for(int j=n-1;j>=0;j--){
-                 int d=matrix[i][j]+dp[i+1][j];
-                 if(j>0)dyl=matrix[i][j]+dp[i+1][j-1];
-                 if(j<n-1)dyr=matrix[i][j]+dp[i+1][j+1];
+        int n = matrix.length;
+        int dp[][] = new int[n][n];
 
-                dp[i][j]=Math.min(d,Math.min(dyl,dyr));
+        // Initialize the bottom row of dp with the corresponding values of matrix
+        for (int j = 0; j < n; j++) {
+            dp[n - 1][j] = matrix[n - 1][j];
+        }
+
+        // Fill the dp array from bottom to top
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                int d = matrix[i][j] + dp[i + 1][j]; // down
+                int dyl = j > 0 ? matrix[i][j] + dp[i + 1][j - 1] : Integer.MAX_VALUE; // down-left
+                int dyr = j < n - 1 ? matrix[i][j] + dp[i + 1][j + 1] : Integer.MAX_VALUE; // down-right
+
+                dp[i][j] = Math.min(d, Math.min(dyl, dyr));
             }
-         } 
-         int min=Integer.MAX_VALUE;
-         for(int j=0;j<n;j++){
-            min=Math.min(min,dp[0][j]);
-         }
+        }
+
+        // Find the minimum value in the first row of dp
+        int min = Integer.MAX_VALUE;
+        for (int j = 0; j < n; j++) {
+            min = Math.min(min, dp[0][j]);
+        }
+
         return min;
     }
 }
