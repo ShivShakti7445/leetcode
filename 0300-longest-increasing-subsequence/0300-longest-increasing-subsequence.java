59817317f499842562f21cd7fe35c0bc9214a4a2
Shiv Shakti <141945568+ShivShakti7445@users.noreply.github.com>
2024-10-14 09:31:39 +0530

    Time: 76 ms (24.63%), Space: 78.7 MB (12.32%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,23 +1,20 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
-        int dp[][]=new int[n][n+1];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n + 1; j++) {
-                dp[i][j] = -1; 
+        int dp[][]=new int[n+1][n+1];
+        for (int i = 0; i <= n; i++) {
+            dp[n][i] = 0; // If we have processed all elements, LIS is 0.
+        }
+        for(int ind=n-1;ind>=0;ind--){
+            for(int pind=ind-1;pind>=-1;pind--){
+                int nottake=0+dp[ind+1][pind+1];
+                int take=0;
+                if(pind==-1||nums[ind]>nums[pind]){
+                    take=1+dp[ind+1][ind+1];
+                }
+                dp[ind][pind+1]=Math.max(take,nottake);
             }
         }
-        return ans(0,-1,nums,n,dp);
-
-    }
-    static int ans(int ind,int pind,int[] nums,int n,int dp[][]){
-        if(ind==n)return 0;
-        if(dp[ind][pind+1]!=-1)return (dp[ind][pind+1]);
-        int nottake=0+ans(ind+1,pind,nums,n,dp);
-        int take=0;
-        if(pind==-1||nums[ind]>nums[pind]){
-            take=1+ans(ind+1,ind,nums,n,dp);
-        }
-        return dp[ind][pind+1]=Math.max(take,nottake);
+        return dp[0][-1+1];
     }
 }
