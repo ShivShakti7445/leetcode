944f5431e419c650ae660e65557fb6dc28a1524a
Shiv Shakti <141945568+ShivShakti7445@users.noreply.github.com>
2024-10-14 09:56:26 +0530

    Time: 48 ms (38.26%), Space: 43.9 MB (87.1%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,20 +1,40 @@
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int n=nums.length;
+//         int dp[][]=new int[n+1][n+1];
+//         for (int i = 0; i <= n; i++) {
+//             dp[n][i] = 0;
+//         }
+//         for(int ind=n-1;ind>=0;ind--){
+//             for(int pind=ind-1;pind>=-1;pind--){
+//                 int nottake=0+dp[ind+1][pind+1];
+//                 int take=0;
+//                 if(pind==-1||nums[ind]>nums[pind]){
+//                     take=1+dp[ind+1][ind+1];
+//                 }
+//                 dp[ind][pind+1]=Math.max(take,nottake);
+//             }
+//         }
+//         return dp[0][-1+1];
+//     }
+// }
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
-        int dp[][]=new int[n+1][n+1];
-        for (int i = 0; i <= n; i++) {
-            dp[n][i] = 0; // If we have processed all elements, LIS is 0.
+        int dp[]=new int[n];
+        for (int i = 0; i <n; i++) {
+            dp[i] = 1;
         }
-        for(int ind=n-1;ind>=0;ind--){
-            for(int pind=ind-1;pind>=-1;pind--){
-                int nottake=0+dp[ind+1][pind+1];
-                int take=0;
-                if(pind==-1||nums[ind]>nums[pind]){
-                    take=1+dp[ind+1][ind+1];
+        int maxi=1;
+        for(int ind=0;ind<n;ind++){
+            for(int pind=0;pind<ind;pind++){
+                if(nums[ind]>nums[pind]){
+                    dp[ind]=Math.max(dp[ind],1+dp[pind]);
+                    maxi=Math.max(maxi,dp[ind]);
                 }
-                dp[ind][pind+1]=Math.max(take,nottake);
             }
         }
-        return dp[0][-1+1];
+        return maxi;
     }
 }
